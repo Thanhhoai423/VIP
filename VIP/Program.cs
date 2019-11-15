efbca2248ecbb0678a7198b9594ea081ec569789
Hero <Hero@DESKTOP-K2K18SP>
2019-11-15 10:08:04 +0700

    Nhóm VIP - Đinh Anh Hùng, Nguyễn Thanh Hoài

---
+++ VIP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bai tap 2 - nhom VIP
+
+        }
+    }
+}
